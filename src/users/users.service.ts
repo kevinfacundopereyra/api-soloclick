e2de8c0cb42ce99125876778b2e291e7f9b6e41f
Zahir Hidalgo <pedrozahir912@gmail.com>
2025-09-21 02:23:46 -0300

    Userservice,Usercontroller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@nestjs/common';
+/* import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../users/schemas/user.schema';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel('User')
+    private readonly userModel: Model<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async create(createUserDto: any): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+}
+ */
+
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { UsersController } from './users.controller';
 
 @Injectable()
 export class UsersService {
