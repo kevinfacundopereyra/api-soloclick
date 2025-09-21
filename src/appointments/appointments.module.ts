6827f21301ee9e57975d1f6870595e6c0047cac9
briannedbala <briannedbala@gmail.com>
2025-09-21 05:10:10 -0300

    Se agregaron las funciones de notificacion

--- src/appointments/appointments.module.ts
+++ src/appointments/appointments.module.ts
@@ -45,15 +45,18 @@ import { Appointment, AppointmentSchema } from './schemas/appointment.schema';
 import { AppointmentService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
 import { Professional, ProfessionalSchema } from '../professionals/schemas/professional.schema';
+import { NotificationsService } from '../notifications/notifications.service';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Appointment', schema: AppointmentSchema },
       { name: 'Professional', schema: ProfessionalSchema },
+      { name: 'User', schema: UserSchema }, // <--- Agrega el esquema de User
     ]),
   ],
-  providers: [AppointmentService],
+  providers: [AppointmentService, NotificationsService],
   controllers: [AppointmentsController],
 })
 export class AppointmentsModule {}
