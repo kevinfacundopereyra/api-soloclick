d121bdd955b77356da01e380223860e0514fc25e
Kevin Pereyra <pereyrakevinfacundo@gmail.com>
2025-09-21 00:28:53 -0300

    appointment duration

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+/* import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AppointmentService } from './appointments.service';
 
 @Controller('appointments')
@@ -13,5 +13,28 @@ export class AppointmentsController {
   @Post()
   create(@Body() createAppointmentDto: any) {
     return this.appointmentsService.create(createAppointmentDto);
+  }
+}
+ */
+
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AppointmentService } from './appointments.service';
+import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { Appointment } from './schemas/appointment.schema';
+
+@Controller('appointments')
+export class AppointmentsController {
+  constructor(private readonly appointmentService: AppointmentService) {}
+
+  @Get()
+  async findAll(): Promise<Appointment[]> {
+    return this.appointmentService.findAll();
+  }
+
+  @Post()
+  async create(
+    @Body() createAppointmentDto: CreateAppointmentDto,
+  ): Promise<Appointment> {
+    return this.appointmentService.create(createAppointmentDto);
   }
 }
