f69676e807a6f361741ff39a56029eab7e8955e0
briannedbala <briannedbala@gmail.com>
2025-10-15 20:42:22 -0300

    Implement payment management: add CreatePaymentDto, Payment schema, and enhance PaymentsController and PaymentsService with payment-related methods

--- src/professionals/professionals.controller.ts
+++ src/professionals/professionals.controller.ts
@@ -1,23 +1,3 @@
-/* import { Controller, Post, Body, Get } from '@nestjs/common';
-import { ProfessionalsService } from './professionals.service';
-import { CreateProfessionalDto } from './dto/create-professional.dto';
-
-@Controller('professionals')
-export class ProfessionalsController {
-  constructor(private readonly professionalsService: ProfessionalsService) {}
-
-  @Get()
-  findAll() {
-    return this.professionalsService.findAll();
-  }
-
-  @Post()
-  create(@Body() createProfessionalDto: CreateProfessionalDto) {
-    return this.professionalsService.create(createProfessionalDto);
-  }
-}
- */
-
 import {
   Controller,
   Post,
